6f5ae456cd5ce64056a022675be1c07b40a228d7
Himanshuxyzz <himanshuxyzz@gmail.com>
2023-09-18 02:07:19 +0530

    add: header ❌

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
+import Head from "./components/Head";
+import Body from "./components/Body";
 
 function App() {
   return (
     <>
-      <h1 className="text-2xl">Hello</h1>
+      <Head />
+      <Body />
     </>
   );
 }
 
 export default App;
+
+{
+  /*
+  ************
+  # Head
+  # Body
+   # Sidebar
+    # Menu Items
+   # Main container
+    # ButtonList or filter
+    # Video container
+    # Video card
+***************
+   */
+}
---
+++ src/components/Head.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div>
+      <img
+        className="w-10 h-10"
+        src="https://vectorified.com/images/hamburger-menu-icon-svg-7.png"
+        alt="hamburger menu"
+      />
+
+      <img
+        className="w-32 h-7"
+        src="https://en.wikifur.com/w/images/9/9c/YouTube_logo.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Head;
